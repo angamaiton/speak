eeb9bd1c777e4977515ae71c07e2e33477d12345
Kjartan Angamaiton <ofurmadur@icloud.com>
2017-02-28 14:25:19 -0600

    initial (with Vue, Bulma)

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login">
+    <section class="section">
+      <div class="container">
+        <h1 class="title">{{ title }}</h1>
+        <hr>
+        <div class="columns">
+          <div class="column is-half">
+            <label class="label">Username</label>
+            <p class="control">
+              <input class="input" type="text" placeholder="Username">
+            </p>
+            <label class="label">Password</label>
+            <p class="control">
+              <input class="input" type="text" placeholder="Password">
+            </p>
+            <a class="button is-primary">Log in</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      title: 'Log in',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="nav has-shadow">
+    <div class="container">
+      <div class="nav-left">
+        <router-link to="/" class="nav-item is-tab" exact>{{title}}</router-link>
+        <router-link to="about" class="nav-item is-tab">About</router-link>
+        <router-link to="hello" class="nav-item is-tab">Test</router-link>
+      </div>
+      <div class="nav-right">
+        <router-link to="login" class="nav-item is-tab">Log in</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data() {
+    return {
+      title: 'Speak',
+    };
+  },
+};
+</script>
